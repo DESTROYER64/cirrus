da0057a431d3c7ff9f5ab9ac16a8c78e0650f3b1
DESTROYER64 <96740555+DESTROYER64@users.noreply.github.com>
2022-04-13 09:54:31 +0530

    Update upload_ccache.sh

--- upload_ccache.sh
+++ upload_ccache.sh
@@ -2,15 +2,14 @@
 
 cd /tmp
 
-# Compress function with pigz for faster compression
-com () 
-{ 
-    echo $(date +"%d-%m-%Y %T")
-    time tar "-I zstd -1 -T2" -cf $1.tar.zst $1
+# Compress function with zstd 
+
+ccache_upload () 
+{
+	echo $(date +"%d-%m-%Y %T")
+	time tar "-I zstd -1 -T2" -cf $1.tar.zst $1
+	rclone copy --drive-chunk-size 256M --stats 1s $1.tar.zst cirrus:ccache/ci2 -P
 }
 
-time com ccache 1 # Compression level 1, its enough
-
-mkdir -p ~/.config/rclone
-echo "$DRIVE" > ~/.config/rclone/rclone.conf
-time rclone copy --drive-chunk-size 256M --stats 1s $1.tar.zst cirrus:ccache/ci2 -P
+cd /tmp
+ccache_upload ccache
